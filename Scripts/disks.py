aa8b1647d47beaa14ba057ffda42c455d4ab307a
BuildTools <unconfigured@null.spigotmc.org>
2024-04-19 18:22:55 +0100

    Added disk

---
+++ Scripts/disks.py
@@ -0,0 +1,25 @@
+import json
+import os
+real_path = os.path.realpath(__file__)
+dir_path = os.path.dirname(real_path)
+def getRegistry(address):
+    with open(f"{dir_path}\\disk.json", "r") as f:
+        jStr = (f.read())
+    j = json.loads(jStr)
+    return j[address] if address in j.keys() else "404ERRORREGISTRYDOESNOTEXIST"
+def setRegistry(address, value):
+    if not value == "404ERRORREGISTRYDOESNOTEXIST":
+        with open(f"{dir_path}\\disk.json", "r") as f:
+            jStr = (f.read())
+        j = json.loads(jStr)
+        j[address] = value
+        with open(f"{dir_path}\\disk.json", "w") as f:
+            f.write(json.dumps(j))
+def list():
+    l = []
+    with open(f"{dir_path}\\disk.json", "r") as f:
+        jStr = (f.read())
+    j = json.loads(jStr)
+    for i in j.keys():
+        l.append(i)
+    return l
