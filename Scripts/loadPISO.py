a4e8e1e0b93e72b55101a1ded3bc0222e92308b9
BuildTools <unconfigured@null.spigotmc.org>
2024-04-27 21:31:24 +0100

    Added .piso (PB8 ISO) support.

---
+++ Scripts/loadPISO.py
@@ -0,0 +1,27 @@
+import os
+import sys
+import json
+
+path = input(".piso Path: ")
+
+real_path = os.path.realpath(__file__)
+dir_path = os.path.dirname(real_path)
+ROM_path = f"{dir_path}\\ROM.pb8"
+files_path = f"{dir_path}\\disk.json"
+
+files = {}
+rom = ""
+
+try:
+    f = open(path, "r")
+    exec(f.read())
+except Exception as e:
+    sys.exit(e)
+finally:
+    f.close()
+
+with open(files_path, "w") as f:
+    f.write(json.dumps(files))
+
+with open(ROM_path, "w") as f:
+    f.write(rom)
